c7162c241a672f3983cfc4ac574dfd28e7014155
darrenhearn <darrenhearnlist@gmail.com>
2015-12-29 09:52:48 -0500

    Update ardutil.c

--- ardutil.c
+++ ardutil.c
@@ -1,8 +1,3 @@
-// This macro (normally part of pins_arduino.h) doesn't seem to exist in the Codebender environment (?)
-#ifndef digitalPinToInterrupt
-#define digitalPinToInterrupt(p)  ((p) == 2 ? 0 : ((p) == 3 ? 1 : -1))
-#endif
-
 byte bcdToDec(byte bcd) {
     return 10*(bcdByte >> 4) + (bcdByte & 0x0f);
 }
