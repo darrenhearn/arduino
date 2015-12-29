30d4509b77104681bc3674a760ff0f2be4e6f1ac
darrenhearn <darrenhearnlist@gmail.com>
2015-12-29 10:33:13 -0500

    Added documentation for BCD functions

--- arduino_util.c
+++ arduino_util.c
@@ -1,8 +1,10 @@
 byte bcdToDec(byte bcd) {
+    // The high (leftmost) nibble encodes the 10's digit; the low (rightmost) nibble contains the 1's digit
     return 10*(bcdByte >> 4) + (bcdByte & 0x0f);
 }
 
 byte decToBcd(byte dec) {
+    // Isolate the 10's digit and push to the high (leftmost) nibble; put the 1's digit in the low (rightmost) nibble
     byte tens = dec / 10;
     byte ones = dec - 10*tens;
     return (tens << 4) | ones;
